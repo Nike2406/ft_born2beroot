5cd1469d7a4ebb48548f2748a8f613da78a94464
prochell <prochell@student.21-school.ru>
2021-06-11 20:48:39 +0300

    upload monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,17 +1,18 @@
-#!/bin/bash
 arch=$(uname -a)
 pcpu=$(lscpu | grep 'CPU(s):' | awk 'NR == 1 {print $2}')
 vcpu=$(nproc)
-ramuse=$(free | grep 'LVMGroup-root' | awk 'NR == 1 {prinf"%d/%.1fG (%d%%)", $3, $2, $5}')
-cpuld=$(mpstat | aek '{printf"%s %s", $3, $4}')
+ramuse=$(free | grep 'Mem:' | awk 'NR == 1 {printf"%d/%dMB (%.2f%%)", $3 / 1024, $2 / 1024, $3 * 100 / $2}')
+romuse=$(df -h | grep 'LVMGroup-root' | awk 'NR == 1 {printf"%d/%.1fG (%d%%)", $3, $2, $5}')
+cpuld=$(mpstat | awk '$13 ~ /[0-9.]+/ {printf"%s", 100 - $13"%"}')
+lstld=$(who -b | awk '{printf"%s %s", $3, $4}')
 lvm=$(lsblk | grep 'lvm' | wc -l)
-tcp=$(netstat -t | wc -l)
+tcp=$(netstat -anp | grep 'ESTAB' | wc -l)
 usrlog=$(who | wc -l)
 ip=$(ifconfig | grep 'inet' | awk 'NR == 1 {print $2}')
 mac=$(ifconfig | grep 'ether' | awk 'NR == 1 {print $2}')
 usudo=$(cat /var/log/sudo/sudo_log | grep 'COMMAND' | wc -l)
 
-if (("$lvm));
+if (("$lvm"));
 then tmp="yes"
 else tmp="no"
 fi
